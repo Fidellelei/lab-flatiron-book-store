8ff32c74d8bbc7f399c75fa595f2621dbe98f2c6
Fidelle <fidellelei788@gmail.com>
2026-04-20 20:39:27 +0300

    Completed Bookstore Manager lab

--- index.js
+++ index.js
@@ -43,5 +43,32 @@ const bookStore = {
     ]
 }
 
-// Write your code here!
+const bookStoreTitle = document.getElementById("header");
+bookStoreTitle.textContent = bookStore.name;
+
+const bookList = document.getElementById("book-list");
+
+bookStore.books.forEach(book => {
+    const bookContainer = document.createElement("li");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const bookImage = document.createElement("img");
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookImage.src = book.imageUrl;
+    bookImage.alt = book.title;
+
+    bookContainer.appendChild(bookTitle);
+    bookContainer.appendChild(bookAuthor);
+    bookContainer.appendChild(bookImage);
+
+    bookList.appendChild(bookContainer);
+});
+
+const deleteItem = document.getElementById("delete-this");
+
+if (deleteItem) {
+    deleteItem.remove();
+}
 
